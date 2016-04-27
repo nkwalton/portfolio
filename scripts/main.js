e9cfa88412d7a3707d61914797828f448d1d657f
Nick <nkwalton@yahoo.com>
2016-04-26 20:53:47 -0400

    add animations, magic.css, start with waypoints, site refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,33 @@
-$('.parallax-inner').localScroll();
+//To get image over text use different z-index or image as a background image on a div
+
+//parallax functionality
+$(document).ready(function () {
+//    $('.parallax-inner').localScroll();
+
+
+
+//hover over project to display a description
+
+    $('#blocJams').hover(function () {
+       $('#text').fadeIn(500), function () {
+            $('#text').fadeOut(500)
+        }
+    });
+    
+
+    
+    //set timer to 7 seconds, after that, load the magic animation and repeat forever
+setInterval(function(){ 
+    $('.ion-headphone, .ion-model-s, .ion-android-boat, .ion-android-laptop, .ion-android-bicycle').toggleClass('magictime vanishIn');
+}, 7000 );
+    
+    //waypoints fun
+    var $backgroundImage = $('.background-image');
+    
+    $backgroundImage.waypoint(function () {
+        console.log('waypoint fun testing!!!');
+    });
+    
+    
+});
+
